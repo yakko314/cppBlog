23b842124aeee8f11c273a52553f89be0b16632c
yakko314 <iacobescu.ionut.256@gmail.com>
2025-06-03 17:10:40 +0300

    making sure that admin app has the same basis as user app

--- src/adminapp.cpp
+++ src/adminapp.cpp
@@ -1,25 +1,50 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <vector>
 #include <unordered_map>
+
+#include <chrono>
+#include <ctime>
+
 
 #include "Comment.h"
 #include "Post.h"
 
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+
 using namespace std;
 
 ifstream f;
-
+ofstream g;
 
 vector<Post> posts;
 unordered_map<int, int> idToIndex;
 
+int nrPosts = 0;
+int nrComments = 0;
 void read() {
 
     f.open("tests/Postari.txt");
-    int n;
-    f >> n;
-    for (int i = 0; i < n; i++) {
+    f >> nrPosts;
+    for (int i = 0; i < nrPosts; i++) {
         int id;
         string title;
         string file;
@@ -45,8 +70,8 @@ void read() {
 
     f.open("tests/Comentarii.txt");
     string content;
-    f >> n;
-    for (int i = 0; i < n; i++) {
+    f >> nrComments;
+    for (int i = 0; i < nrComments; i++) {
         int x;
         f>>x;
         f.ignore();
@@ -66,8 +91,9 @@ void read() {
     f.close();
 
     f.open("tests/Statistici.txt");
-    f >> n;
-    for (int i = 0; i < n; i++) {
+    int nrInteractiuni;
+    f >> nrInteractiuni;
+    for (int i = 0; i < nrInteractiuni; i++) {
         int x;
         f>>x;
         if (idToIndex.count(x)) {
@@ -86,24 +112,34 @@ void read() {
 }
 
 void showAll() {
+    int i = 1;
     for (auto const &p : posts) {
-        cout << p << '\n';
+        cout << i++ << " | " << p << '\n';
     }
 }
 void showAllExpanded() {
     for (auto& p : posts) {
-        // cout << p << '\n';
-        // // for (const auto& c : p.getComments()) {
-        // //     cout << c << '\n';
-        // // }
         p.bigPrint(cout);
         cout << '\n';
     }
 }
 
-int main() {
+void cmdHelp() {
+    cout << CYAN "~Sistem de blog: Admin~\n" RESET
+    << GREEN "etc" RESET "- etcetcetc";
+}
+void noPost() {
+    cout << RED "Postarea nu exista!\n" RESET;
+}
+
+int main(int argc, char* argv[]){
     read();
-    showAll();
-    showAllExpanded();
-    return 0;
+    if (argc == 1) {
+        cmdHelp();
+        return 0;
+    }
+    string command = argv[1];
+    if (command == "etcetc") {
+
+    }
 }
